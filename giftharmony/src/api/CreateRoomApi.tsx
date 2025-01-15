46fc64ebd1227d7d66b43037004551ab8366efde
Jiranthanin-Supakad <jiranthanin_su@cmu.ac.th>
2025-01-15 17:38:03 +0700

    remove unuse code

--- giftharmony/src/api/CreateRoomApi.tsx
+++ giftharmony/src/api/CreateRoomApi.tsx
@@ -3,13 +3,6 @@ import axios from "axios";
 const base_url = import.meta.env.VITE_API_URL;
 
 export const CreateRoomApi = {
-  // getRoom: async () => {
-  //   const response = await axios.get(`${base_url}/rooms`, {
-  //     withCredentials: true, 
-  //   });
-  //   return response.data;
-  // },
-
   createRoom: async (data: { name: string }) => {
     const response = await axios.post(
       `${base_url}/createRoom`,
