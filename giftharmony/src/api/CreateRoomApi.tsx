008277301c0a3af9d15d34b9e89d6487914774ae
Jiranthanin-Supakad <jiranthanin_su@cmu.ac.th>
2025-01-15 17:39:24 +0700

    add interface

--- giftharmony/src/api/CreateRoomApi.tsx
+++ giftharmony/src/api/CreateRoomApi.tsx
@@ -1,9 +1,10 @@
 import axios from "axios";
+import { ICreateRoom } from "../interface/ICreateRoom";
 
 const base_url = import.meta.env.VITE_API_URL;
 
 export const CreateRoomApi = {
-  createRoom: async (data: { name: string }) => {
+  createRoom: async (data: ICreateRoom) => {
     const response = await axios.post(
       `${base_url}/createRoom`,
       data, 
